41a63f9d2a334b5b91ab16ea2df5ced0c2d6954d
Miguel Carvalho <emece.pt@gmail.com>
2016-03-24 18:53:01 +0000

    View initial commit

--- BeneathTheCodeSky/src/org/bootcamp/hackathon/beneaththecodesky/Main.java
+++ BeneathTheCodeSky/src/org/bootcamp/hackathon/beneaththecodesky/Main.java
@@ -9,4 +9,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("peixotas up in the air!!!");
     }
+
+
 }
---
+++ BeneathTheCodeSky/src/org/bootcamp/hackathon/beneaththecodesky/view/deleteme.java
@@ -0,0 +1,7 @@
+package org.bootcamp.hackathon.beneaththecodesky.view;
+
+/**
+ * Created by Mike on 24/03/16.
+ */
+public class deleteme {
+}
