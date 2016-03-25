0c2d5814ffbb36a9cf3b179111d92c97680fe74a
Sérgio Gouveia <sergiogouveia@protonmail.ch>
2016-03-25 05:32:50 +0000

    mega parser

--- BeneathTheCodeSky/src/peixotas/TesterMain.java
+++ BeneathTheCodeSky/src/peixotas/TesterMain.java
@@ -1,7 +1,9 @@
 package peixotas;
 
+import peixotas.terminal.Global;
 import peixotas.terminal.Parser;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,19 +15,12 @@ public class TesterMain {
     public static void main(String[] args) {
 
         Parser parser = new Parser();
+        Global g = new Global();
 
+        List<String[]> parsingResult = parser.parse("     tester.print1  \n print.this \n and.this \ntester.print2");
 
-       // Map<String, String> parsingResult = parser.parseLine("     tester.print1  ");
-//        for (Map<String, String> instruction : parsingResult.values()) {
-//            for (String text : instruction.values()) {
-//                System.out.print(text + " ");
-//            }
-//            System.out.println();
-//        }
-     //   System.out.println(parsingResult.size());
-
-
-
+        System.out.println("PARSING RESULT : " + parsingResult.get(1)[0]);
+        g.compare(parsingResult);
     }
 
 }
