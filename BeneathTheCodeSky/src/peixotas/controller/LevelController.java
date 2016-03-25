4eded6cb573a86a71505faf50a9acedae7d53d93
Miguel Carvalho <emece.pt@gmail.com>
2016-03-25 08:30:08 +0000

    it runs!!

--- BeneathTheCodeSky/src/peixotas/controller/LevelController.java
+++ BeneathTheCodeSky/src/peixotas/controller/LevelController.java
@@ -1,21 +1,19 @@
 package peixotas.controller;
 
-import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.util.Duration;
-import peixotas.model.Game;
 import peixotas.model.Player;
 import peixotas.model.interactable_objects.InteractableObject;
 import peixotas.model.levels.Level;
-
-import javafx.geometry.Rectangle2D;
-import peixotas.view.SpriteAnimation;
 
 import java.util.ArrayList;
 
@@ -31,7 +29,23 @@ public abstract class LevelController implements Controller {
     private Scene scene;
     //private Rectangle2D rectangle2D;
     //private Animation animation;
+
+    final DoubleProperty velocity = new SimpleDoubleProperty();
+    final LongProperty lastUpdateTime = new SimpleLongProperty();
     private ArrayList<ImageView> views = new ArrayList<>();
+    final AnimationTimer animation = new AnimationTimer() {
+        @Override
+        public void handle(long timestamp) {
+            if (lastUpdateTime.get() > 0) {
+                final double elapsedSeconds = (timestamp - lastUpdateTime.get()) / 1_000_000_000.0;
+                final double deltaX = elapsedSeconds * velocity.get();
+                final double oldX = playerView.getTranslateX();
+                final double newX = Math.max(0, Math.min(1200, oldX + deltaX));
+                playerView.setTranslateX(newX);
+            }
+            lastUpdateTime.set(timestamp);
+        }
+    };
 
     public void setPane(Pane pane) {
         this.pane = pane;
@@ -42,12 +56,28 @@ public abstract class LevelController implements Controller {
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                System.out.println("key pressed: " + event.getText());
-                double dx = playerView.getX() + 100;
-                playerView.setX(dx);
+
+                switch (event.getCode()) {
+                    case RIGHT:
+                        velocity.set(100);
+                        playerView.setScaleX(-1);
+                        break;
+                    case LEFT:
+                        velocity.set(-100);
+                        playerView.setScaleX(1);
+                        break;
+                }
             }
         });
 
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                velocity.set(0);
+            }
+        });
+
+        animation.start();
         ArrayList<InteractableObject> gameObjects = level.getGameObjects();
 
         for (InteractableObject obj : gameObjects) {
