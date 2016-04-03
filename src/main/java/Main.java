301e2b6adc9fc2338d94d40b5e31dc71a4d78b29
Joana <joana@MacBook-ProjP.home>
2016-04-03 18:15:18 +0100

    first commit, git ignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by jPeixoto on 15/03/16.
+ */
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    public void start(Stage primaryStage) throws Exception {
+
+
+        Navigation.getInstance().setStage(primaryStage);
+        primaryStage.setTitle("Beneath the CodeSky");
+        primaryStage.show();
+        Navigation.getInstance().loadScreen("initial");
+
+
+
+    }
+}
---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by joana on 4/3/16.
+ */
+public class Controller implements Initializable{
+
+
+        @FXML
+        private Pane pane;
+
+        @FXML
+        private ImageView boxImage;
+
+        public void initialize(URL location, ResourceBundle resources) {
+                
+        }
+}
